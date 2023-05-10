4cd6a11f5c8187f1b49938bf21b240b5661a8842
James Chok <james.chok1000+github@gmail.com>
2023-05-10 16:14:39 +0100

    updating BernsteinLegendre to have the same interface as Bernstein

--- Approximators/BernsteinLegendre/utils.py
+++ Approximators/BernsteinLegendre/utils.py
@@ -2,6 +2,25 @@ import numpy as np
 from scipy.special import gammaln
 
 
+def BernsteinPolynomial(n, x):
+    working_mask = (x != 0) * (x != 1)
+
+    k = np.arange(0, n + 1)
+    log_B = np.zeros((n + 1, len(x)))
+
+    log_B[:, working_mask] += combination(n, k, as_log=True)[:, None]
+    log_B[:, working_mask] += k[:, None] * np.log(x[None, working_mask])
+    log_B[:, working_mask] += (n - k[:, None]) * np.log(1 - x[None, working_mask])
+
+    B = np.exp(log_B) * working_mask[None, :]
+
+    # Edge cases
+    B[0, x == 0] = 1
+    B[n, x == 1] = 1
+
+    return B
+
+
 def combination(n, k, as_log=False):
     if as_log:
         return gammaln(n + 1) - gammaln(n - k + 1) - gammaln(k + 1)
