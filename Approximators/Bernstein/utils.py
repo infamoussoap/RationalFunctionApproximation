70b37cbaef4317446d3eae43b19907e9aa6a0de4
James Chok <james.chok1000+github@gmail.com>
2023-05-11 09:15:51 +0100

    Chebyshev spacing type was wrong

--- Approximators/Bernstein/utils.py
+++ Approximators/Bernstein/utils.py
@@ -50,7 +50,7 @@ def spacing(spacing_type='linear', n_points=100):
     if spacing_type.lower() == 'linear':
         return np.linspace(0, 1, n_points)
     elif spacing_type.lower() == 'chebyshev':
-        return (np.cos(np.linspace(-np.pi, np.pi, n_points)) + 1) / 2
+        return (np.cos(np.linspace(np.pi, 0, n_points)) + 1) / 2
 
     raise ValueError("Invalid spacing_type. Select one from ['linear', 'chebyshev']")
 
