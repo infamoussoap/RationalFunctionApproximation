f4418213321250abd37fb236b1324a93c0afb5b7
James Chok <james.chok1000+github@gmail.com>
2023-05-18 19:20:32 +0100

    adding rational approximator abc class

---
+++ Approximators/RationalApproximator.py
@@ -0,0 +1,18 @@
+import abc
+
+
+class RationalApproximator(abc.ABC):
+    @abc.abstractmethod
+    def __init__(*args, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def numerator(self, x):
+        pass
+
+    @abc.abstractmethod
+    def denominator(self, x):
+        pass
+
+    def __call__(self, x):
+        return self.numerator(x) / self.denominator(x)
