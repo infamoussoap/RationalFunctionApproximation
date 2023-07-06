2df922acd2ffe994b9a3412f82e61236181321a5
James Chok <james.chok1000+github@gmail.com>
2023-07-06 16:38:40 +0100

    Gradient of bernstein approximator can be computed

--- Approximators/Bernstein/BernsteinApproximator.py
+++ Approximators/Bernstein/BernsteinApproximator.py
@@ -6,7 +6,7 @@ from numpy.polynomial.legendre import Legendre
 from .ArmijoSearch import ArmijoSearch
 
 from ..utils import bernstein_to_legendre_matrix, bernstein_to_chebyshev_matrix
-from ..Polynomials import BernsteinPolynomial
+from ..Polynomials import BernsteinPolynomial, LegendrePolynomial
 from ..validation_checks import check_bernstein_w, check_X_in_range
 from ..RationalApproximator import RationalApproximator
 
@@ -14,32 +14,49 @@ from ..RationalApproximator import RationalApproximator
 class BernsteinApproximator(ArmijoSearch, RationalApproximator, ABC):
     def __init__(self):
         self.w = None
+
+        self.n = None
         self.m = None
+
         self._legendre_coef = None
 
-    def denominator(self, x):
+    def denominator(self, x, grad=False):
         check_X_in_range(x, 0, 1)
 
         if len(self.w) == 1:
             return np.ones_like(x)
 
-        return self.w @ BernsteinPolynomial(self.m, x)
+        return self.w @ BernsteinPolynomial(self.m, x, grad=grad)
 
-    def numerator(self, x):
+    def numerator(self, x, grad=False):
         check_X_in_range(x, 0, 1)
 
-        numerator_vals = [np.ones_like(x) if len(coef) == 1 else Legendre(coef, domain=[0, 1])(x)
-                          for coef in self._legendre_coef]
+        numerator_vals = self._eval_numerator(x, grad=grad)
 
         if len(numerator_vals) == 1:
             return numerator_vals[0]
         return numerator_vals
 
+    def _eval_numerator(self, x, grad=False):
+        if grad:
+            return self._numerator_grad(x)
+
+        numerator_vals = [np.ones_like(x) if len(coef) == 1 else Legendre(coef, domain=[0, 1])(x)
+                          for coef in self._legendre_coef]
+        return numerator_vals
+
+    def _numerator_grad(self, x):
+        numerator_grads = [c @ LegendrePolynomial(self.n, x, grad=True) for c in self._legendre_coef]
+        return numerator_grads
+
     def reset(self, w=None):
         self.w = check_bernstein_w(w, self.m + 1)
 
-    def __call__(self, x):
+    def __call__(self, x, grad=False):
         check_X_in_range(x, 0, 1)
+
+        if grad:
+            return self._grad(x)
 
         denominator = self.denominator(x)
         numerator = self.numerator(x)
@@ -48,6 +65,20 @@ class BernsteinApproximator(ArmijoSearch, RationalApproximator, ABC):
             return numerator / denominator
         else:
             return [num / denominator for num in numerator]
+
+    def _grad(self, x):
+        numerator_vals = self._eval_numerator(x, grad=False)
+        numerator_grads = self._eval_numerator(x, grad=True)
+
+        denominator_val = self.denominator(x, grad=False)
+        denominator_grad = self.denominator(x, grad=True)
+
+        grads = [(numerator_grad * denominator_val - numerator_val * denominator_grad) / (denominator_val ** 2)
+                 for (numerator_val, numerator_grad) in zip(numerator_vals, numerator_grads)]
+
+        if len(grads) == 1:
+            return grads[0]
+        return grads
 
     @property
     def legendre_coef(self):
