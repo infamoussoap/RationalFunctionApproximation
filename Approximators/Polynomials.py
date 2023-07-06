fe4deae4f8990a05c90421534e45a126d6d89eda
James Chok <james.chok1000+github@gmail.com>
2023-07-06 15:59:21 +0100

    polynomials now have grad

--- Approximators/Polynomials.py
+++ Approximators/Polynomials.py
@@ -1,18 +1,36 @@
 import numpy as np
 from scipy.special import eval_legendre, eval_chebyt
+from scipy.special import legendre, chebyt
 
 from .utils import combination
 
 
-def LegendrePolynomial(n, x):
+def LegendrePolynomial(n, x, grad=False):
+    if grad:
+        return legendre_derivative(n, x)
+
     return np.vstack([eval_legendre(i, 2 * x - 1) for i in range(n + 1)])
 
 
-def ChebyshevPolynomial(n, x):
+def legendre_derivative(n, x):
+    return np.array([legendre(i).deriv()(2 * x - 1) for i in range(n + 1)])
+
+
+def ChebyshevPolynomial(n, x, grad=False):
+    if grad:
+        return chebyshev_derivative(n, x)
+
     return np.vstack([eval_chebyt(i, 2 * x - 1) for i in range(n + 1)])
 
 
-def BernsteinPolynomial(n, x):
+def chebyshev_derivative(n, x):
+    return np.array([chebyt(i).deriv()(2 * x - 1) for i in range(n + 1)])
+
+
+def BernsteinPolynomial(n, x, grad=False):
+    if grad:
+        return bernstein_derivative(n, x)
+
     working_mask = (x != 0) * (x != 1)
 
     k = np.arange(0, n + 1)
@@ -29,3 +47,27 @@ def BernsteinPolynomial(n, x):
     B[n, x == 1] = 1
 
     return B
+
+
+def bernstein_derivative(n, x):
+    log_derivative = np.zeros((n + 1, len(x)))
+
+    a = np.arange(n + 1)
+
+    working_mask = (0 < x) * (x < 1)
+
+    log_derivative += combination(n, a, as_log=True)[:, None]
+
+    log_derivative[:-1, working_mask] += (n - a[:-1, None] - 1) * np.log(1 - x[None, working_mask])
+    log_derivative[1:, working_mask] += (a[1:, None] - 1) * np.log(x[None, working_mask])
+
+    derivative = np.exp(log_derivative)
+
+    derivative[:-2, x == 1] = 0
+    derivative[2:, x == 0] = 0
+
+    derivative[0, :] *= -n
+    derivative[-1, :] *= n
+    derivative[1:-1, :] *= (a[1:-1, None] - n * x[None, :])
+
+    return derivative
