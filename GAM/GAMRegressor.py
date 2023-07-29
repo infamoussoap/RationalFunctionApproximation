e37eb3aa7b8faa39565d3881135eaf01f0ad172d
James Chok <james.chok1000+github@gmail.com>
2023-07-29 16:21:14 +0100

    added support for interaction terms via multivariate bernstein

--- GAM/GAMRegressor.py
+++ GAM/GAMRegressor.py
@@ -7,6 +7,7 @@ from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
 import Approximators
 from Approximators.Bernstein import CauchySimplex as Bernstein
+from Approximators.MultivariateBernstein import CauchySimplex as MultivariateBernstein
 
 from .WriteToScreen import WriteToScreen
 
@@ -17,14 +18,18 @@ Approximators.ignore_warnings()
 
 
 class GAMRegressor(RegressorMixin, BaseEstimator):
-    def __init__(self, rational_degrees=(2, 2), stopping_tol=1e-6, max_iter=10, bernstein_max_iter=10,
-                 bernstein_stopping_tol=1e-10, w=None, c1=1e-4, c2=0.5, line_search_iter=100, gamma=0.1, verbose=False):
+    def __init__(self, rational_degrees=(2, 2), interaction_terms=None, stopping_tol=1e-6, max_iter=10,
+                 bernstein_max_iter=10, bernstein_stopping_tol=1e-10, w=None,
+                 c1=1e-4, c2=0.5, line_search_iter=100, gamma=0.1, verbose=False):
         """ Initialize a generalized additive error model where each function is a rational function
 
         Parameters
         ----------
         rational_degrees : 2-tuple or list of 2-tuple, default (2, 2)
             Defines the rational degrees for each function. If a 2-tuple, then it is applied to each function.
+        interaction_terms : list[tuple(int)], default None
+            List of interaction terms. Note that any term in the interaction terms will not have their own
+            rational function
         stopping_tol : float, default 1e-6
             The stopping tolerance to stop the backfitting procedure
         max_iter : int, default=10
@@ -51,6 +56,7 @@ class GAMRegressor(RegressorMixin, BaseEstimator):
             If set to true then the result of each step will be printed.
         """
         self.rational_degrees = rational_degrees
+        self.interaction_terms = [] if interaction_terms is None else interaction_terms
 
         self.stopping_tol = stopping_tol
         self.max_iter = max_iter
@@ -107,13 +113,29 @@ class GAMRegressor(RegressorMixin, BaseEstimator):
         self.n_features_in_ = X.shape[1]
         self.intercept_ = np.mean(y)
 
-        rational_degrees = check_rational_degrees(self.rational_degrees, self.n_features_in_)
-        self._learner_functions = [Bernstein(n, m, max_iter=self.bernstein_max_iter,
-                                             stopping_tol=self.bernstein_stopping_tol,
-                                             w=self.w, c1=self.c1, c2=self.c2,
-                                             line_search_iter=self.line_search_iter, gamma=self.gamma)
-                                   for (n, m) in rational_degrees]
-        self._learner_functions_constants = np.zeros(self.n_features_in_)
+        unique_interaction_terms = np.unique([x for terms in self.interaction_terms for x in terms])
+        non_interaction_terms = [i for i in range(self.n_features_in_) if i not in unique_interaction_terms]
+        self._terms_list = non_interaction_terms + self.interaction_terms
+
+        rational_degrees = check_rational_degrees(self.rational_degrees, len(self._terms_list))
+
+        self._learner_functions = []
+        for i, term in enumerate(self._terms_list):
+            n, m = rational_degrees[i]
+            if isinstance(term, int):
+                self._learner_functions.append(Bernstein(n, m, max_iter=self.bernstein_max_iter,
+                                                         stopping_tol=self.bernstein_stopping_tol,
+                                                         w=self.w, c1=self.c1, c2=self.c2,
+                                                         line_search_iter=self.line_search_iter, gamma=self.gamma))
+            else:
+                n_vals = [n] * len(term)
+                m_vals = [m] * len(term)
+                self._learner_functions.append(MultivariateBernstein(n_vals, m_vals, max_iter=self.bernstein_max_iter,
+                                                                     stopping_tol=self.bernstein_stopping_tol,
+                                                                     w=self.w, c1=self.c1, c2=self.c2,
+                                                                     line_search_iter=self.line_search_iter,
+                                                                     gamma=self.gamma))
+        self._learner_functions_constants = np.zeros(len(self._terms_list))
 
         prev_loss = 0
         current_loss = np.inf
@@ -141,22 +163,26 @@ class GAMRegressor(RegressorMixin, BaseEstimator):
         return self
 
     def _fit(self, X, y):
-        for i in range(self.n_features_in_):
-            learner_function_predictions = [f(X[:, j]) + c if f.w is not None else np.zeros_like(X[:, j])
-                                            for j, (f, c) in
-                                            enumerate(zip(self._learner_functions, self._learner_functions_constants))
+        for i, terms in enumerate(self._terms_list):
+            learner_function_predictions = [f(X[:, terms]) + c if f.w is not None else np.zeros(len(X))
+                                            for j, (terms, f, c) in
+                                            enumerate(zip(self._terms_list,
+                                                          self._learner_functions,
+                                                          self._learner_functions_constants))
                                             if j != i]
             target_y = y - self.intercept_ - np.sum(learner_function_predictions, axis=0)
 
-            self._learner_functions[i].fit(X[:, i], target_y)
-            self._learner_functions_constants[i] = -np.mean(self._learner_functions[i](X[:, i]))
+            self._learner_functions[i].fit(X[:, terms], target_y)
+            self._learner_functions_constants[i] = -np.mean(self._learner_functions[i](X[:, terms]))
 
     def predict(self, X):
         check_is_fitted(self)
 
         X = check_array(X)
 
-        learner_function_predictions = [f(X[:, j]) + c if f.w is not None else np.zeros_like(X[:, j])
-                                        for j, (f, c) in
-                                        enumerate(zip(self._learner_functions, self._learner_functions_constants))]
+        learner_function_predictions = [f(X[:, terms]) + c if f.w is not None else np.zeros(len(X))
+                                        for terms, f, c in
+                                        zip(self._terms_list,
+                                            self._learner_functions,
+                                            self._learner_functions_constants)]
         return self.intercept_ + np.sum(learner_function_predictions, axis=0)
