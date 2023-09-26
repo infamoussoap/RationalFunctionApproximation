7ed4f06b6ca47fc444dff47f01cb462005781a0f
James Chok <james.chok1000+github@gmail.com>
2023-09-26 11:08:05 +0100

    All approximators that inheret from RationalApproximator (which should be all) now has an evaluate method

--- Approximators/RationalApproximator.py
+++ Approximators/RationalApproximator.py
@@ -1,5 +1,12 @@
 import abc
 
+import numpy as np
+
+from .WriteToScreen import WriterToScreen
+
+
+DEFAULT_WRITER = WriterToScreen()
+
 
 class RationalApproximator(abc.ABC):
     @abc.abstractmethod
@@ -23,3 +30,15 @@ class RationalApproximator(abc.ABC):
     @abc.abstractmethod
     def fit(self, X, y):
         pass
+
+    def evaluate(self, X, y):
+        predicted_y = self(X)
+
+        if isinstance(predicted_y, np.ndarray):
+            diffs = [predicted_y - y]
+        else:
+            diffs = [pred_y - true_y for pred_y, true_y in zip(predicted_y, y)]
+
+        DEFAULT_WRITER.write_norms("Errors", diffs, norms=[2, np.inf])
+        print()
+
